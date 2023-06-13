2ab5a3e4696900078f739b0df0cce2f8c2abeabf
changwoolab <cwyoo01@naver.com>
2023-06-14 00:20:07 +0900

    Remove Total Volume Credits 1: Split loop

--- refactoring/ch1/example.js
+++ refactoring/ch1/example.js
@@ -6,14 +6,16 @@ function statement(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // print line for this order
     result += ` ${playFor(perf).name}: ${usd(thismount / 100)} (${
       perf.audience
     })`;
     totalAmount += amountFor(perf);
   }
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
