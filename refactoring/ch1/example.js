7b6482b59aa1ace7511a30055ddd71563a5b358e
changwoolab <cwyoo01@naver.com>
2023-06-14 00:22:16 +0900

    Remove Total Volume Credits 3: Extract function and apply Inline Variables

--- refactoring/ch1/example.js
+++ refactoring/ch1/example.js
@@ -11,14 +11,18 @@ function statement(invoice, plays) {
     })`;
     totalAmount += amountFor(perf);
   }
+
+  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
+  return result;
+}
+
+function totalVolumeCredits() {
   let volumeCredits = 0;
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
   }
-
-  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
+  return volumeCredits;
 }
 
 function usd(aNumber) {
