14be026e9154050f3958d9f40d51c0a426d308e6
changwoolab <cwyoo01@naver.com>
2023-06-15 00:47:56 +0900

    Split Phase 2: Move data from renderPlainText from statement
    To make renderPlainText operate solely on data passed to it

--- refactoring/ch1/example.js
+++ refactoring/ch1/example.js
@@ -2,12 +2,14 @@
 
 function statement(invoice, plays) {
   const statementData = {};
-  return renderPlainText(statementData, invoice, plays);
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
-  for (let perf of invoice.performances) {
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`;
+  for (let perf of data.performances) {
     result += ` ${playFor(perf).name}: ${usd(thismount / 100)} (${
       perf.audience
     })`;
@@ -18,7 +20,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
@@ -26,7 +28,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
