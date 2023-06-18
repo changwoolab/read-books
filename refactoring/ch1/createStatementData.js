53934547ed4a5fb591d0b51597e86328d65d0935
changwoolab <cwyoo01@naver.com>
2023-06-18 23:09:39 +0900

    Replace Conditional with Polymorphism 7: apply polymorphism to credits
    most plays expect to check if audience is above 30 -> leave the more common case on the superclass as a default and override it as necessary

--- refactoring/ch1/createStatementData.js
+++ refactoring/ch1/createStatementData.js
@@ -26,6 +26,10 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performance.audience;
   }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
+  }
 }
 
 class PerformanceCalculator {
@@ -39,11 +43,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ("comedy" === this.play.type)
-      result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
