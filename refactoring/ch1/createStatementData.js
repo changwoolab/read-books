9aaf391f560edd40fadb7f7b618b9aa0cfe18f59
changwoolab <cwyoo01@naver.com>
2023-06-18 23:06:25 +0900

    Replace Conditional with Polymorphism 6: apply polymorphism to amount

--- refactoring/ch1/createStatementData.js
+++ refactoring/ch1/createStatementData.js
@@ -1,5 +1,31 @@
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`Unknown type: ${aPlay.type}`);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30)
+      result += 1000 * (this.performance.audience - 30);
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      thisAmount += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+  }
 }
 
 class PerformanceCalculator {
@@ -9,25 +35,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          thisAmount += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${this.play.type}`);
-    }
-    return result;
+    throw new Error("subclass responsibility");
   }
 
   get volumeCredits() {
